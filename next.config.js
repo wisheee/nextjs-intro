5a7bdef38fcdcabd5d7bd0fd6918f5f59cf603b0
wisheee <wishee.u@gmail.com>
2022-03-13 18:51:43 +0900

    feat: 인기 영화 목록 API 추가

--- next.config.js
+++ next.config.js
@@ -4,6 +4,23 @@ const nextConfig = {
   images: {
     domains: ['image.tmdb.org'],
   },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/contact',
+  //       destination: '/form',
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies/popular',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+      },
+    ];
+  }
 }
 
 module.exports = nextConfig
