7a66db5869145331882d7445a84460d98534a5da
wisheee <wishee.u@gmail.com>
2022-03-13 22:56:18 +0900

    feat: fetch data -> Server Side Rendering으로 수정

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,11 @@
-import Image from "next/image";
-import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (
-        await fetch('/api/movies/popular')
-      ).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map(movie => <Movie key={movie.id} movie={movie} />)}
+      {results?.map(movie => <Movie key={movie.id} movie={movie} />)}
       <style jsx>{`
         .container {
           display: grid;
@@ -28,4 +16,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`${process.env.VERCEL_URL}/api/movies/popular`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
