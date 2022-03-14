0f60fcd01c5fc85a200cf8967998f245b90af169
wisheee <wishee.u@gmail.com>
2022-03-14 17:13:37 +0900

    feat: 404 page 추가

---
+++ pages/404.js
@@ -0,0 +1,22 @@
+export default function NotFound() {
+  return (
+    <div>
+      😥
+      <span>404</span>
+      <h3>페이지를 찾을 수 없습니다</h3>
+      <style jsx>{`
+        div {
+          padding-top: 5rem;
+          text-align: center;
+          font-size: 4rem;
+        }
+        span {
+          display: block;
+        }
+        h3 {
+          margin-top: 1rem;
+        }
+      `}</style>
+    </div>
+  );
+}
