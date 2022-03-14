c778b911861da60b71b2d702fbc26332545c15fe
wisheee <wishee.u@gmail.com>
2022-03-14 17:13:22 +0900

    feat: 영화 상세보기 API 추가

---
+++ components/movies/Detail.js
@@ -0,0 +1,12 @@
+import styles from "../../styles/Movie.module.scss";
+
+export default function Detail({ movie }) {
+  console.log(movie);
+  return (
+    <div className={styles.wrap}>
+      <figure className={styles.img}>
+      </figure>
+      <h4 className={styles.title} title={movie.original_title}>{movie.original_title}</h4>
+    </div>
+  );
+}
--- pages/movies/[...params].js
+++ pages/movies/[...params].js
@@ -1,12 +1,32 @@
-import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import Detail from "../../components/movies/Detail";
+import Seo from "../../components/Seo";
 
-export default function Detail() {
-  const router = useRouter();
-  const [title, id] = router.query.params;
-
+export default function MovieDetail({ params }) {
+  const [title, id] = params;
+  const [movie, setMovie] = useState();
+  useEffect(() => {
+    (async () => {
+      const data = await (
+        await fetch(`/api/movies/${id}`)
+      ).json();
+      setMovie(data);
+    })();
+  }, [id]);
+  
   return (
     <div>
+      <Seo title={title} />
+      {movie && <Detail movie={movie} />}
       <h4>{title || "Loading..."}</h4>
     </div>
   );
+}
+
+export function getServerSideProps({ params: { params } }) {
+  return {
+    props: {
+      params,
+    },
+  };
 }
