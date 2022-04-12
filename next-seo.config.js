a2006189202142779054131d0449eb9b796e9d7d
wisheee <wishee.u@gmail.com>
2022-04-12 17:25:01 +0900

    feat: DefaultSeo 컴포넌트 추가

---
+++ next-seo.config.js
@@ -0,0 +1,13 @@
+const nextSeoConfig = {
+  title: 'Wishee 영화관',
+  description: 'themoviedb API를 활용한 영화 정보 웹 서비스입니다',
+  openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    url: 'https://nextjs-intro-orpin.vercel.app/',
+    title: 'Wishee 영화관',
+    description: 'themoviedb API를 활용한 영화 정보 웹 서비스입니다'
+  }
+};
+
+export default nextSeoConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
-import Layout from '../components/Layout'
-import '../styles/globals.scss'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import Layout from '../components/Layout';
+import { DefaultSeo } from 'next-seo';
+import SEO from '../next-seo.config';
+import '../styles/globals.scss';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </Layout>
   )
